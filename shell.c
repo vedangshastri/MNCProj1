05afa8ef5225760566d8e9d0ab64402957b650d2
vedangshastri <vedangshastri@users.noreply.github.com>
2015-10-11 08:13:50 -0400

    Modifications done... Parsing success

--- shell.c
+++ shell.c
@@ -3,9 +3,13 @@
 
 int main()
 {
-    int i,k,spc_count=0;
-    char comm[20],para1[20],para2[20],choice[20];
+    int i,k,spc_count=0,j;
+    char comm[30],para1[20],para2[20],choice[20];
     scanf("%[^\n]s",comm);
+	j=strlen(comm);
+	memset(para1,0,20);
+	memset(para2,0,20);
+	memset(choice,0,20);
    	for(i=0;i<=strlen(comm);i++)
 						{comm[i]=tolower(comm[i]);}
 					for(i=0;i<strlen(comm);i++)
@@ -17,29 +21,23 @@ int main()
 							spc_count++;
 							strncpy(choice,comm,i);
 							k=i+1;
-							printf("%s",choice);
+							
 							break;
 						}
-					  
+					  	
 						}
 						else
 						
 					    {
 						while(comm[i]==' ')
 							{
-								strncpy(para1,comm,3);
-								strncpy(para2,comm,2);
+								strncpy(para1,comm+k,i-k);
+								
+								strncpy(para2,comm+i+1,j-i-1);
 								break;
 							}
 
 						}
 					}
-                    printf("\n%s\t%s\t%sd",choice,para1,para2);
+                    printf("\n%s_%s_%s\n",choice,para1,para2);
 }
-
-
-
-
-
-
-
